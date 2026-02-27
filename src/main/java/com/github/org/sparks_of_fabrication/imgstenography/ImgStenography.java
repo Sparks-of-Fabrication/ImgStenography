1e8c11295e9b83c12d05c12a35c226509586301d
PetkoPetkov03 <komandir1612@gmail.com>
2026-02-27 11:32:53 +0200

    initial commit

---
+++ src/main/java/com/github/org/sparks_of_fabrication/imgstenography/ImgStenography.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.github.org.sparks_of_fabrication.imgstenography;
+
+/**
+ *
+ * @author petko
+ */
+public class ImgStenography {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
